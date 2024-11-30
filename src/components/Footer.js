4b0616678a88858531504a510039c75ab1289382
Robby_Malunga_Jr <46316955+iamrobbiejr@users.noreply.github.com>
2024-11-30 15:07:28 +0200

    redesigned singup - left with sorting bgs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
+        <>
+        {/*<MusicPlayer />*/}
         <footer className="bg-gray-800 text-white py-4 mt-auto">
             <div className="container mx-auto px-4 text-center">
                 <p>&copy; 2023 Fan-Exclusive Streaming Platform. All rights reserved.</p>
             </div>
         </footer>
+            </>
     );
 };
 
